3ae3e7bf56dae11134062881b165548c1764091a
ssmmoo1 <sammy.ochoa115@gmail.com>
2019-07-22 20:17:46 -0500

    Update mapGenerator.py

--- mapGenerator.py
+++ mapGenerator.py
@@ -6,8 +6,8 @@ import pyperclip
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+Tk().withdraw() 
+filename = askopenfilename() 
 
 
 map = Image.open(filename)
